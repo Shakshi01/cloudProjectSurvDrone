c2baa459bf15fe448f2ff36925ed0084bfefbffb
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2023-04-26 14:38:30 -0700

    Add files via upload

--- src/App2Components/scenes/deleteAllMissions/index.jsx
+++ src/App2Components/scenes/deleteAllMissions/index.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+import axios from "axios";
 
+
+function DeleteAllMissionPlans() {
+
+    return (
+        <div className="delete-all-missions-button">
+            <h3>Delete all Maps</h3>
+            <form onSubmit={(e) => {
+                axios.delete("http://localhost:5001/api/deleteAllMissionPlans");
+            }}>
+                <button type="submit">Delete All Mission Plans</button>
+            </form>
+        </div>    
+    )
+}
+
+export default DeleteAllMissionPlans;

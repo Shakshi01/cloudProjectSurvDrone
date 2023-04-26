34fb4113c6c2497ecc033518a7b7b6965e310ff9
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2023-04-26 14:31:40 -0700

    updated addMap.jsx

--- src/App2Components/scenes/addMap/index.jsx
+++ src/App2Components/scenes/addMap/index.jsx
@@ -1,1 +1,63 @@
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import {Box} from "@mui/material";
+import {Formik} from "formik";
+import Header from "../../components/Header";
+import { useNavigate } from 'react-router-dom';
 
+function AddMapForm() {
+
+    const navigate = useNavigate();
+
+    const [inputData, setInputData] = useState({
+        Name:"",
+        MapImage: ""
+    });
+
+
+    const handleChange = (e) => {
+        setInputData((prev) => ({
+            ...prev,
+            [e.target.name]:e.target.value,
+        }))
+    }
+
+
+    const sendRequest = async() => {
+        await axios.post('http://localhost:5001/api/addMap', {
+            Name: inputData.Name,
+            MapImage: inputData.MapImage
+        })
+        .then((res) => {console.log(res);})
+        .catch(err => console.log(err));
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputData);
+        sendRequest().then(() => alert("Added map succesfully!!"))
+        .then(() => navigate("/dashboard"));
+    };
+
+    
+    return (
+        <Box m="20px">
+            <Header title="Add new Farm Map" />
+                <Formik onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit}>
+                        <label for="Name">Map Name:</label><br />
+                        <input type="text" id="Name" name="Name" value={inputData.Name} onChange={handleChange} /><br />
+                        <br />
+                        <label for="MapImage">Map Image URL:</label><br />
+                        <input type="text" id="MapImage" name="MapImage" value={inputData.MapImage} onChange={handleChange} /><br />
+                        <br />
+                        <input type="submit" value="Add New Map" />
+                    </form>
+                </Formik>
+        </Box>
+    )
+}
+
+export default AddMapForm;

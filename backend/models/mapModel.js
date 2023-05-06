959f4899c9cb6a9573ecc00149f3dd11497863e5
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2023-05-06 03:36:49 -0700

    updated model for map schema

--- backend/models/mapModel.js
+++ backend/models/mapModel.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
 
 const mapSchema = new mongoose.Schema({
+    TenantId: {
+        type: String,
+        required: true
+    },
     Name: {
         type: String,
         required: true
     },
-    MapImage: {
-        type: String, // save image as URL may change to image
+    Address: {
+        type: String,
+        required: true
+    },
+    Lat: {
+        type: Float32Array,
+        required: true
+    },
+    Long: {
+        type: Float32Array,
         required: true
     }
 })

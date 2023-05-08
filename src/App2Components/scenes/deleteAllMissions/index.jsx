92feccf7a099b1e3bed40831c8d2aed52ae9eb0b
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2023-05-07 23:40:04 -0700

    Delete All missions support for multi-tenancy

--- src/App2Components/scenes/deleteAllMissions/index.jsx
+++ src/App2Components/scenes/deleteAllMissions/index.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+import TenantIdSingleton from "../../components/TenantId";
 
 
 function DeleteAllMissionPlans() {
+
+    const navigate = useNavigate();
+
+
+    const sendRequest = async(id) => {
+        await axios.delete(`http://localhost:5001/api/deleteAllMissionPlans/${TenantIdSingleton.id}`)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch(err => console.log(err));
+    }
 
     return (
         <div className="delete-all-missions-button">
             <h3>Delete all Maps</h3>
             <form onSubmit={(e) => {
-                axios.delete("http://localhost:5001/api/deleteAllMissionPlans");
+                sendRequest()
+                .then(() => navigate("/dashboard"));
             }}>
                 <button type="submit">Delete All Mission Plans</button>
             </form>
